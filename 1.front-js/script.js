961c97a9df97e8223c3785622ed22bc946fb7bc6
AnnaFeona <glavatskikh.anna@gmail.com>
2023-09-22 20:31:38 +0300

    style: add css to project

--- 1.front-js/script.js
+++ 1.front-js/script.js
@@ -4,12 +4,12 @@ const API_URL_MOCK = `https://fanated.com/coingecko/mock`;
 
 const body = document.querySelector('body');
 const main = createEl('main', body, ['main', 'wrapper']);
-const table = createEl('table', main);
+const table = createEl('table', main, ['table']);
 
 createTable();
 
 async function getData() {
-  const responce = await fetch(API_URL, { method: 'GET'});
+  const responce = await fetch(API_URL, { method: 'GET', headers: {}});
   if (!responce.ok) {
     const message = await responce.text();
 
@@ -39,10 +39,10 @@ async function createTable() {
 
   currTable.forEach((item, index) => {
     const { id, name, symbol } = item;
-    const row = createEl('tr', table);
-    createEl('td', row).textContent = id;
-    createEl('td', row).textContent = symbol;
-    createEl('td', row).textContent = name;
+    const row = createEl('tr', table, ['table__row']);
+    createEl('td', row, ['table__cell']).textContent = name;
+    createEl('td', row, ['table__cell']).textContent = id;
+    createEl('td', row, ['table__cell']).textContent = symbol;
 
     if (index < 5) {
       row.style.backgroundColor = 'blue';
